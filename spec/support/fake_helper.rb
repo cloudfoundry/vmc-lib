03d8f2916143701054b936c064a4b57d1df66989
Alex Suraci <suraci.alex@gmail.com>
2012-12-18 14:35:48 -0800

    prevent issue with attrs that can be false or nil
    Change-Id: Ib677d349e7947f5cc70e35541aaef72b75c2ad84

--- spec/support/fake_helper.rb
+++ spec/support/fake_helper.rb
@@ -17,6 +17,16 @@ module Fake
 
       objs
     end
+
+    def fake_model(name = :my_fake_model, &init)
+      klass = Class.new(CFoundry::V2::FakeModel) do
+        self.object_name = name
+      end
+
+      klass.class_eval(&init) if init
+
+      klass
+    end
   end
 
   def fake(attributes = {})
@@ -151,6 +161,26 @@ module CFoundry::V2
           setup_reverse_relationship(x)
         end
       end
+    end
+  end
+
+
+  class FakeModel < CFoundry::V2::Model
+    attr_reader :diff
+
+    class << self
+      attr_writer :object_name
+    end
+  end
+
+
+  module ModelMagic
+    def self.define_client_methods(&blk)
+      FakeClient.module_eval(&blk)
+    end
+
+    def self.define_base_client_methods(&blk)
+      FakeBaseClient.module_eval(&blk)
     end
   end
 
