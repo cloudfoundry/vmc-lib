2b8d779c7c03badd084d808d94393b4fa8b50a67
Elisabeth Hendrickson and Matthew Boedicker <mboedicker@rbcon.com>
2013-01-07 12:24:33 -0800

    Use cf-uaa-lib gem in uaa client.
    Change-Id: Ib3a8397c4fdbc14e626d22f8c755bfe544c0524b

--- lib/cfoundry/uaaclient.rb
+++ lib/cfoundry/uaaclient.rb
@@ -1,4 +1,5 @@
 require "cfoundry/baseclient"
+require 'uaa'
 
 module CFoundry
   class UAAClient < BaseClient
@@ -6,116 +7,59 @@ module CFoundry
 
     def initialize(
         target = "https://uaa.cloudfoundry.com",
-        client_id = "vmc")
+            client_id = "vmc")
       @target = target
       @client_id = client_id
       @redirect_uri = "https://uaa.cloudfoundry.com/redirect/vmc"
+      @token_issuer = CF::UAA::TokenIssuer.new(target, client_id)
     end
 
     def prompts
-      get("login", :accept => :json)[:prompts]
+      symbolize_keys(CF::UAA::Misc.server(target)['prompts'])
     end
 
     def authorize(credentials)
-      query = {
-        :client_id => @client_id,
-        :response_type => "token",
-        :redirect_uri => @redirect_uri
-      }
-
-      auth =
-        post(
-          { :credentials => credentials },
-          "oauth", "authorize",
-          :return_response => true,
-          :content => :form,
-          :accept => :json,
-          :params => query)
-
-      case auth
-      when Net::HTTPRedirection
-        extract_token(auth["location"])
-      else
-        json = parse_json(auth.body)
-        raise CFoundry::Denied.new(
-          auth.code.to_i,
-          json[:error_description])
+      begin
+        response = @token_issuer.implicit_grant_with_creds(credentials)
+        "#{response.info['token_type']} #{response.info['access_token']}"
+      rescue CF::UAA::BadResponse => e
+        # Handle other types of bad responses?
+        raise case e.message
+                when 'status 401' then
+                  CFoundry::Denied.new(401, 'Bad credentials')
+                else
+                  e
+              end
       end
     end
 
     def users
-      get("Users", :accept => :json)
+      CF::UAA::Scim.new(target, token).query(:user)
     end
 
     def change_password(guid, new, old)
-      put(
-        { :password => new, :oldPassword => old },
-        "Users", guid, "password",
-        :accept => :json,
-        :content => :json)
+      CF::UAA::Scim.new(target, token).change_password(guid, new, old)
     end
 
     def password_score(password)
-      response = post(
-        { :password => password },
-        "password", "score",
-        :content => :form,
-        :accept => :json
-      )
-      required_score = response[:requiredScore] || 0
-      case (response[:score] || 0)
-        when 10 then :strong
-        when required_score..9 then :good
-        else :weak
+      response = CF::UAA::Misc.password_strength(target, password)
+
+      required_score = response['requiredScore'] || 0
+      case (response['score'] || 0)
+        when 10 then
+          :strong
+        when required_score..9 then
+          :good
+        else
+          :weak
       end
     end
 
     private
 
-    def handle_response(response, accept)
-      case response
-      when Net::HTTPSuccess, Net::HTTPRedirection
-        if accept == :json
-          if response.is_a?(Net::HTTPNoContent)
-            raise CFoundry::BadResponse.new(
-              204,
-              "Expected JSON response, got 204 No Content")
-          end
-
-          parse_json(response.body)
-        else
-          response.body
-        end
-
-      when Net::HTTPBadRequest, Net::HTTPUnauthorized, Net::HTTPForbidden
-        info = parse_json(response.body)
-        raise Denied.new(response.code, info[:error_description])
-
-      when Net::HTTPNotFound
-        raise NotFound
-
-      when Net::HTTPConflict
-        info = parse_json(response.body)
-        raise CFoundry::Denied.new(response.code, info[:message])
-
-      else
-        raise BadResponse.new(response.code, response.body)
-      end
+    def symbolize_keys(h)
+      h.inject({}) { |memo, (k, v)| memo[k.to_sym] = v; memo }
     end
 
-    def extract_token(url)
-      _, params = url.split('#')
-      return unless params
-
-      values = {}
-      params.split("&").each do |pair|
-        key, val = pair.split("=")
-        values[key] = val
-      end
-
-      return unless values["access_token"] && values["token_type"]
-
-      "#{values["token_type"]} #{values["access_token"]}"
-    end
   end
 end
